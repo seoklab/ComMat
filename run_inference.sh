a7f9c4fd57ef662ad994846df6f19270a72910ec
YubeenKim <oilbeen@snu.ac.kr>
2024-08-24 03:56:47 +0900

    change sh file and add Af2rank

--- run_inference.sh
+++ run_inference.sh
@@ -14,4 +14,4 @@ export NCCL_P2P_DISABLE=1
 
 python scripts/inference_end_to_end_w_igfold.py --batch_size 1 --weight community_size_32.pt --test_seed_size 32 \
     --fasta_path 7sn1_H_L_#.fasta --test_n_recycle 8 --test_n_crop 100 --test_ulr_type H_3 \
-    --output_path test_0821 --using_post_kabsch --write_pdb
+    --output_path test_0824 --using_post_kabsch --write_pdb
